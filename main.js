b23fa330348634c13c551e8bef5b471ccc3b2a9a
Maxime-Moilliet <mllt.dev66@gmail.com>
2022-05-24 10:30:40 +0200

    add: init starter

---
+++ main.js
@@ -0,0 +1,34 @@
+let letterList = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+// Code
+function firstPage() {}
+function previous() {}
+function nextPage() {}
+function lastPage() {}
